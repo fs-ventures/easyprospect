eae8f00258144dc8b27b51056ef332f7563b405a
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-06-07 12:15:13 -0500

    update of all app folders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_only, :except => :show
-  
+  layout "users"
 
   def index
     @users = User.all
